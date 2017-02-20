b8e489e82e26ff2d92afb54641038d40469c9fbb
Palash Dhande <pd-153273@hs-weingarten.de>
2017-02-20 13:12:02 +0100

    Add comments

--- ms-net.py
+++ ms-net.py
@@ -7,6 +7,11 @@ import math
 import scipy.io
 import matplotlib.pyplot as plt
 
+#only one branch : depth branch
+#input : low resolution depth map
+#output : high resolution depth map
+#relevant functions : preprocessing, feature_extraction, upsampling, reconstruction
+
 def ms_net():
 	
 	preprocessing()
