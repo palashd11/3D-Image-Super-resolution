c6081c06fbe81e6c5dfa34dbb6bcd0ad4a2a1f56
Palash Dhande <pd-153273@hs-weingarten.de>
2017-02-20 12:19:33 +0100

    First commit

---
+++ msg-net.py
@@ -0,0 +1,44 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import tensorflow as tf
+import numpy as np
+import math
+import scipy.io
+import matplotlib.pyplot as plt
+
+def convolution():
+	#inputs are kernel and high-frequency image
+	#output is the convolution vector or feature map
+	
+
+def deconvolution():
+
+def preprocessing():
+
+def pooling():
+
+def prelu():
+
+def lowpass():
+
+def subimages():
+
+def fusion():
+
+def reconstruction():
+
+def upsampling():
+	#call deconvolution function on a kernel a feature map
+	#add the result of the above to a bias vector
+	#call activation funtion(prelu) on the above and return the output
+
+def feature_extraction():
+
+
+
+
+#define cost function
+#set learning rate
+#set optimizer
+#minimize cost function
